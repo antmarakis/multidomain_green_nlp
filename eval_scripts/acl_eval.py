6960d9b773c9796fc9b3c81798886897d3b8f8c2
Antonis Maronikolakis <marakis.anthony@gmail.com>
2021-03-08 15:45:52 +0100

    Update acl_eval.py

--- eval_scripts/acl_eval.py
+++ eval_scripts/acl_eval.py
@@ -14,8 +14,7 @@ test_df = pd.read_csv('acl_arc_test.csv')
 def run_test(lm):
     model = ClassificationModel('distilbert', lm, num_labels=6, args={'overwrite_output_dir': True, 'fp16': False, 'num_train_epochs': 2, 'wandb_project': None})
     model.train_model(train_df)
-
-    result, model_outputs, predictions = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
+    result, model_outputs, wrong_preds = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
     return result
 
 
