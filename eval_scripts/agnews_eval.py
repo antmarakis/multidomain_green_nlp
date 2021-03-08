c9107b720d0b7113e7e16cfab3fe834720623d92
Antonis Maronikolakis <marakis.anthony@gmail.com>
2021-03-08 15:46:11 +0100

    Update agnews_eval.py

--- eval_scripts/agnews_eval.py
+++ eval_scripts/agnews_eval.py
@@ -19,10 +19,8 @@ def run_test(lm):
     model = ClassificationModel('distilbert', lm, num_labels=4,
                                 args={'overwrite_output_dir': True, 'fp16': False,
                                 'save_steps':-1})
-
-    # Train the model
     model.train_model(train_df)
-    result, model_outputs, predictions = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
+    result, model_outputs, wrong_preds = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
 
     return result
 
