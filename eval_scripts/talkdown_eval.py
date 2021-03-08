d75c9f8c26f98063b97e8bb06c926cbaaa88fceb
Antonis Maronikolakis <marakis.anthony@gmail.com>
2021-03-08 15:45:44 +0100

    Update talkdown_eval.py

--- eval_scripts/talkdown_eval.py
+++ eval_scripts/talkdown_eval.py
@@ -14,8 +14,7 @@ test_df = pd.read_csv('talkdown_test.csv')
 def run_test(lm):
     model = ClassificationModel('distilbert', lm, num_labels=2, args={'overwrite_output_dir': True, 'fp16': False, 'num_train_epochs': 2})
     model.train_model(train_df)
-
-    result, model_outputs, predictions = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
+    result, model_outputs, wrong_preds = model.eval_model(test_df, f1=f1_multiclass, acc=accuracy_score)
 
     return result
 
